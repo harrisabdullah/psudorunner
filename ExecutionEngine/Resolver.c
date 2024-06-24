242368003d207e9c09e560ab86f070e4130bd960
Harris <harrisjan06@gmail.com>
2024-06-24 12:43:02 +0800

    added "NOT_EQUALS" to Resover ( I forgot it )

--- ExecutionEngine/Resolver.c
+++ ExecutionEngine/Resolver.c
@@ -104,7 +104,7 @@ void resolveExpression(struct List* namespace, struct Expression* expression, st
             result->data.boolean = stackPop(stack)->data.boolean && stackPop(stack)->data.boolean; 
         } else if (expression->type == OR) {
             // Makes sure both values get popped.
-            // If it was inline then when the first value resovles to 1 it doesnt bother computing the second.
+            // If this was inline then when the first value resovles to 1 it doesnt bother computing the second.
             // I spent days finding this stupid bug.
             int leftValue = stackPop(stack)->data.boolean;
             int rightValue = stackPop(stack)->data.boolean;
@@ -142,10 +142,14 @@ void resolveExpression(struct List* namespace, struct Expression* expression, st
         return;
     }
 
-    if (expression->type == EQUALS){
+    if (expression->type == EQUALS || expression->type == NOT_EQUALS){
         result->type = BOOLEAN;
         if (leftType == BOOLEAN && rightType == BOOLEAN){
-            result->data.boolean = stackPop(stack)->data.boolean == stackPop(stack)->data.boolean;
+            if (expression->type == EQUALS){
+                result->data.boolean = stackPop(stack)->data.boolean == stackPop(stack)->data.boolean;
+            } else {
+                result->data.boolean = stackPop(stack)->data.boolean != stackPop(stack)->data.boolean;
+            }
             stackPush(stack, result);
             return;
         }
@@ -163,7 +167,11 @@ void resolveExpression(struct List* namespace, struct Expression* expression, st
         } else {
             rightValue = stackPop(stack)->data.real;
         }
-        result->data.boolean = leftValue == rightValue;
+        if (expression->type == EQUALS){
+            result->data.boolean = leftValue == rightValue;
+        } else {
+            result->data.boolean = leftValue != rightValue;
+        }
         stackPush(stack, result);
         return;
     }
@@ -244,97 +252,4 @@ void resolveExpression(struct List* namespace, struct Expression* expression, st
     }
 
     stackPush(stack, result);
-
-//     if (expression->type == NOT){
-//         struct VariableValue* result = malloc(sizeof(struct VariableValue));
-//         result->type = BOOLEAN;
-//         result->data.boolean = !resolveExpression(namespace, expression->right);
-//         return result;
-//     }
-
-//     struct VariableValue* left = resolveExpression(namespace, expression->left);
-//     struct VariableValue* right = resolveExpression(namespace, expression->right);
-//     struct VariableValue* result = malloc(sizeof(struct VariableValue));
-
-//     float leftValue = left->type == REAL? left->data.real:left->data.integer;
-//     float rightValue = right->type == REAL? right->data.real:right->data.integer;
-
-//     if (expression->type == DIVISION){
-//       result->type = REAL;
-//       result->data.real = leftValue / rightValue;
-//         return result;
-//     };
-
-//     if (left->type == REAL || right->type == REAL){
-//         result->type = REAL;
-//     } else {
-//         result->type = INTEGER;
-//     }
-
-//     switch (expression->type) {
-//         case ADDITION:
-//             if (result->type == REAL){
-//                 result->data.real = leftValue + rightValue;
-//                 break;
-//             }
-//             result->data.integer = (int)(leftValue + rightValue);
-//             break;
-
-//         case SUBTRACTION:
-//             if (result->type == REAL){
-//                 result->data.real = leftValue - rightValue;
-//                 break;
-//             }
-//             result->data.integer = (int)(leftValue - rightValue);
-//             break;
-
-//         case MULTIPLICATION:
-//             if (result->type == REAL){
-//                 result->data.real = leftValue * rightValue;
-//                 break;
-//             }
-//             result->data.integer = (int)(leftValue * rightValue);
-//             break;
-
-//         case EQUALS:
-//             result->type = BOOLEAN;
-//             result->data.boolean = (float)(leftValue) == (float)(rightValue);
-//             break;
-
-//         case NOT_EQUALS:
-//             result->type = BOOLEAN;
-//             result->data.boolean = (float)(leftValue) != (float)(rightValue);
-//             break;
-
-//         case GREATER:
-//             result->type = BOOLEAN;
-//             result->data.boolean = (float)(leftValue) > (float)(rightValue);
-//             break;
-
-//         case GREATER_OR_EQUALS:
-//             result->type = BOOLEAN;
-//             result->data.boolean = (float)(leftValue) >= (float)(rightValue);
-//             break;
-
-//         case LESSER:
-//             result->type = BOOLEAN;
-//             result->data.boolean = (float)(leftValue) < (float)(rightValue);
-//             break;
-
-//         case LESSER_OR_EQUALS:
-//             result->type = BOOLEAN;
-//             result->data.boolean = (float)(leftValue) <= (float)(rightValue);
-//             break;
-
-//         case AND:
-//             result->type = BOOLEAN;
-//             result->data.boolean = left->data.boolean && right->data.boolean;
-//             break;
-
-//         case OR:
-//             result->type = BOOLEAN;
-//             result->data.boolean = left->data.boolean || right->data.boolean;
-//             break;
-//     }
-//     return result;
  }
--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "common/tokenTypeToString.h"
 
 int main() {
-   struct List* tokens = tokenize("OUTPUT (1=1 OR 1=2) AND 1=1", 27); // "OUTUPT (1 < 2 OR 1 = 2) AND 3 <= 7"
+   struct List* tokens = tokenize("OUTPUT 2<>2", 11); // "OUTUPT (1 < 2 OR 1 = 2) AND 3 <= 7"
    printTokenList(tokens);
    struct List* AST = parse(tokens, 0, NULL_TYPE);
    printASTList(AST);
