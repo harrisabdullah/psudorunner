e7a204d264e5bde05e628d74d1fcecc4ab97ab8b
Harris <harrisjan06@gmail.com>
2024-06-25 15:23:38 +0800

    added a gitignore

--- build/CMakeFiles/psudo_interprator_2.dir/common/tokenTypeToString.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/psudo_interprator_2.dir/common/tokenTypeToString.c.o: \
-  /Users/harrisabdullah/Documents/vscode_projs/c/common/tokenTypeToString.c \
-  /Users/harrisabdullah/Documents/vscode_projs/c/common/tokenTypeToString.h \
-  /Users/harrisabdullah/Documents/vscode_projs/c/common/../Tokenisation/Token.h
--- build/CMakeFiles/psudo_interprator_2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for psudo_interprator_2.
-# This may be replaced when dependencies are built.
