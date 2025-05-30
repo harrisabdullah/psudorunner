aa83bbba9034c19d7d5a194e1dbc90460a5cf1b2
Harris <harrisjan06@gmail.com>
2025-05-30 19:29:09 +0800

    Test: add unit test for keyword tokenization

--- test/Tokenizer.cpp
+++ test/Tokenizer.cpp
@@ -5,13 +5,31 @@ extern "C" {
   #include "Tokenisation/Tokenizer.h"
 }
 
-TEST(Tokenizer, oneChar) {
+TEST(Tokenizer, OneChar) {
   const char* code = "+-/*():=<,>[]\n";
   TokenType expected[] = {
     ADDITION, SUBTRACTION, DIVISION, MULTIPLICATION,
     OPEN_PAREN, CLOSE_PAREN, COLON, EQUALS, LESSER,
     COMMA, GREATER, OPEN_SQUARE_PAREN, CLOSE_SQUARE_PAREN,
     NEW_LINE
+  };
+
+  List result = tokenize(const_cast<char*>(code), strlen(code));
+  ASSERT_TOKEN_LIST_EQ(result, expected);
+}
+
+TEST(Tokenizer, Keywords) {
+  const char* code =
+    "IF ELSE THEN ENDIF <- <> MOD <= >= OR AND NOT "
+    "BOOLEAN INTEGER REAL STRING OUTPUT DECLARE WHILE ENDWHILE "
+    "REPEAT UNTIL FOR TO NEXT";
+  TokenType expected[] = {
+    IF, ELSE, THEN, ENDIF,
+    ASSIGNMENT, NOT_EQUALS, MODULO, LESSER_OR_EQUALS, GREATER_OR_EQUALS,
+    OR, AND, NOT,
+    BOOLEAN_IDENTIFIER, INTEGER_IDENTIFIER, REAL_IDENTIFIER, STRING_IDENTIFIER,
+    OUTPUT, DECLARE, WHILE, ENDWHILE,
+    REPEAT, UNTIL, FOR, TO, NEXT
   };
 
   List result = tokenize(const_cast<char*>(code), strlen(code));
